552f710d2a579025e3b71f463a371c208d7466a5
Aaron Erb <erbnlegend@gmail.com>
2022-10-26 14:19:16 -0500

    Add Styles and validation. Alter HTML for layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mockSignUpPage
 A mock sign up page to showcase forms
+Photo by <a href="https://unsplash.com/@marcsm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marc Sendra Martorell</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
---
+++ src/script.js
@@ -0,0 +1,28 @@
+const submit = document.getElementById('submit');
+const error = document.getElementById('error');
+const password = document.getElementById('password');
+const checkPassword = document.getElementById('checkPassword')
+
+
+const validation = (e) => {
+    e.preventDefault
+    const passwordField = password.value;
+    const checkPasswordField = checkPassword.value;
+    if(typeof passwordField !== "string") {
+        error.textContent = "Please re-enter password";
+        password.style.borderColor = "#ff0000";
+        checkPassword.style.borderColor = "#ff0000";
+    }
+    else if(passwordField !== checkPasswordField) {
+        error.textContent = "Passwords do not match!";
+        password.style.borderColor = "#ff0000";
+        checkPassword.style.borderColor = "#ff0000";
+    }
+    else if(passwordField === checkPasswordField) {
+        error.textContent = "Match!";
+        password.style.borderColor = "#34515B";
+        checkPassword.style.borderColor = "#34515B";
+    }
+}
+
+submit.addEventListener('click', validation);
